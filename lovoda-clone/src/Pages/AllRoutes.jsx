d61af50a3d1f4fb90406df5387017943bdfdd83a
ships97 <shiprasingh4796@gmail.com>
2022-09-28 21:44:54 +0530

    product page almost done

--- lovoda-clone/src/Pages/AllRoutes.jsx
+++ lovoda-clone/src/Pages/AllRoutes.jsx
@@ -1,10 +1,15 @@
 import React from 'react';
+import { Route, Routes } from "react-router-dom";
+import Productpage from './Productpage';
 
 const AllRoutes = () => {
     return (
         <div>
-            AllRoutes
-        </div>
+            All Routes
+            {/* <Routes>
+            <Route path='/newarrivals' element={<Productpage />} />
+            </Routes> */}
+        </div> 
     );
 };
 
--- lovoda-clone/src/Pages/Productpage.jsx
+++ lovoda-clone/src/Pages/Productpage.jsx
@@ -1,9 +1,34 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
+import Filter from '../Components/Filter';
 
 const Productpage = () => {
+    const [data, setData] = useState([]);
+
+    useEffect(() => {
+        fetch("https://lovodaapi.herokuapp.com/api/newarrivals")
+        .then((res) => res.json())
+        .then((res)=>{
+            setData(res);
+        })
+    },[]);
+
   return (
-    <div>
-      
+    <div style={{border: "1px solid red", width:"80%", margin:"auto"}}>
+        <h1 style={{marginRight: "90%", fontWeight:"normal"}}>New</h1>
+        <Filter/>
+        <div style={{ border: "1px solid black", display:"grid", gridTemplateColumns: "repeat(auto-fit, minmax(240px, 4fr))", gap: "10px",justifyItems: "center", marginTop:"15px" }}>
+            { data?.map((item) => {
+                return(
+                    <div style={{ border:"1px solid green", height: "480px", width: "100%" }}>
+                        <div>
+                           <img src={`${item.image}`} alt="image" style={{ width: "100%", height: "400px" }}/>
+                        </div>
+                        <div>{item.name}</div>
+                        <div>${item.price}.00</div>
+                    </div>
+                )
+            })}
+        </div>
     </div>
   )
 }
