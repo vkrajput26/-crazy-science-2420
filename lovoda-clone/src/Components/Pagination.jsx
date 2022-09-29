7f84543ecf15a33c5ee52fcd1923a65a41aa3ebe
ships97 <shiprasingh4796@gmail.com>
2022-09-30 00:06:27 +0530

    filtering and sorting done

---
+++ lovoda-clone/src/Components/Pagination.jsx
@@ -0,0 +1,12 @@
+import { ArrowBackIcon, ArrowForwardIcon } from "@chakra-ui/icons"
+import { Button, Flex } from "@chakra-ui/react"
+
+export const Pagination = ({handlePage,page}) => {
+    return (
+        <Flex justifyContent={"center"} gap="0.5rem" alignItems={"center"} p="2">
+            <Button disabled={page === 1} onClick={handlePage} rightIcon={<ArrowBackIcon
+            />} colorScheme='teal' variant='outline'>Prev</Button>
+            <Button disabled={page===2} onClick={handlePage} rightIcon={<ArrowForwardIcon />} colorScheme='teal' variant='outline'>Next</Button>
+        </Flex>
+    );
+};
